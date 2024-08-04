4a2df68a6de42f09c62c80a9e46bcdbd310cea37
Female-coder <Magadanineo@gmail.com>
2024-08-04 15:48:36 +0200

    airbnb clone-v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,50 +1,50 @@
 #!/usr/bin/python3
-""" Flask Application """
+"""
+This module defines the Flask application.
+
+The application is defined as a Flask instance. It includes the setup for
+different routes and error handlers.
+"""
+
+from flask import Flask, jsonify
+import os
+
 from models import storage
 from api.v1.views import app_views
-from os import environ
-from flask import Flask, render_template, make_response, jsonify
 from flask_cors import CORS
-from flasgger import Swagger
-from flasgger.utils import swag_from
+
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
-
-@app.teardown_appcontext
-def close_db(error):
-    """ Close Storage """
-    storage.close()
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """ 404 Error
-    ---
-    responses:
-      404:
-        description: a resource was not found
     """
-    return make_response(jsonify({'error': "Not found"}), 404)
+    This function is called when a 404 error occurs.
+    Args:
+      error (Exception): The error that caused the 404.
+    Returns:
+        A JSON response with the content {"error": "Not found"}
+        and a ststus code of 404.
+    """ 
+ return jsonify({"error": "Not found"}), 404
 
+@app.teardown_appcontext
+def close_db(error):
+    """
+    This function is called when the Flask application context is torn down.
 
-app.config['SWAGGER'] = {
-    'title': 'AirBnB clone Restful API',
-    'uiversion': 3
-}
+    Args:
+        error (Exception): The error that caused the context teardown, if any.
 
-Swagger(app)
+    It closes the storage system session.
+    """
+    storage.close()
 
 
 if __name__ == "__main__":
-    """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
 """
-Creating my flask app
+This module sets up the default route and additional routes for the API.
 """
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
-
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def status():
-    '''returns a JSON: "status": "OK"'''
+    """
+    This function handles the route /status and returns a JSON response.
+    """
     return jsonify({"status": "OK"})
 
-
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'])
 def stats():
-    '''endpoint that retrieves the number of each objects by type:'''
-    stats = {
-        'amenities': storage.count('Amenity'),
-        'cities': storage.count('City'),
-        'places': storage.count('Place'),
-        'reviews': storage.count('Review'),
-        'states': storage.count('States'),
-        'users': storage.count('User'),
-    }
-    return jsonify(stats)
+    """
+    This function handles the route /stats and returns a JSON response with the
+    count of each object by type.
+    """
+    return jsonify({
+        "users": storage.count("User"),
+        "places": storage.count("Place"),
+        "cities": storage.count("City"),
+        "states": storage.count("State"),
+        "amenities": storage.count("Amenity"),
+        "reviews": storage.count("Review")
+    })
